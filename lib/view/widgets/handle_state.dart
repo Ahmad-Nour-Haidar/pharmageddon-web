f09b77977d9b083d895dd38890c00d31215bc653
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2023-12-19 01:05:16 +0300

    add AwesomeDialog

--- lib/view/widgets/handle_state.dart
+++ lib/view/widgets/handle_state.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'package:fluttertoast/fluttertoast.dart';
-
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:pharmageddon_web/core/constant/app_size.dart';
 import '../../core/class/parent_state.dart';
-import '../../core/constant/app_color.dart';
 
 void handleState({
   required ParentState state,
@@ -13,78 +12,61 @@ void handleState({
   switch (state.runtimeType) {
     case OfflineState:
       {
-        Fluttertoast.showToast(
-          msg: state.message,
-          fontSize: 18,
-          backgroundColor: AppColor.red,
-          gravity: ToastGravity.TOP,
+        showAwesomeHandleState(
+          context: context,
+          title: state.message,
+          dialogType: DialogType.error,
         );
-        // CustomSnackBar(
-        //   context: context,
-        //   typeSnackBar: TypeSnackBar.error,
-        //   message: state.message,
-        // ).show();
         return;
       }
     case ServerFailureState:
       {
-        Fluttertoast.showToast(
-          msg: state.message,
-          fontSize: 18,
-          backgroundColor: AppColor.red,
-          gravity: ToastGravity.TOP,
+        showAwesomeHandleState(
+          context: context,
+          title: state.message,
+          dialogType: DialogType.error,
         );
-        // CustomSnackBar(
-        //   context: context,
-        //   typeSnackBar: TypeSnackBar.error,
-        //   message: state.message,
-        // ).show();
         return;
       }
     case FailureState:
       {
-        Fluttertoast.showToast(
-          msg: state.message,
-          fontSize: 18,
-          backgroundColor: AppColor.red,
-          gravity: ToastGravity.TOP,
+        showAwesomeHandleState(
+          context: context,
+          title: state.message,
+          dialogType: DialogType.error,
         );
-        // CustomSnackBar(
-        //   context: context,
-        //   typeSnackBar: TypeSnackBar.warning,
-        //   message: state.message,
-        // ).show();
         return;
       }
     case SuccessState:
       {
-        Fluttertoast.showToast(
-          msg: state.message,
-          fontSize: 18,
-          backgroundColor: AppColor.green,
-          gravity: ToastGravity.TOP,
+        showAwesomeHandleState(
+          context: context,
+          title: state.message,
+          dialogType: DialogType.success,
         );
-        // CustomSnackBar(
-        //   context: context,
-        //   typeSnackBar: TypeSnackBar.success,
-        //   message: state.message,
-        // ).show();
         return;
       }
     case WarningState:
       {
-        Fluttertoast.showToast(
-          msg: state.message,
-          fontSize: 18,
-          backgroundColor: AppColor.contentColorYellow,
-          gravity: ToastGravity.TOP,
+        showAwesomeHandleState(
+          context: context,
+          title: state.message,
+          dialogType: DialogType.warning,
         );
-        // CustomSnackBar(
-        //   context: context,
-        //   typeSnackBar: TypeSnackBar.warning,
-        //   message: state.message,
-        // ).show();
         return;
       }
   }
+}
+
+void showAwesomeHandleState({
+  required BuildContext context,
+  required String title,
+  required DialogType dialogType,
+}) {
+  AwesomeDialog(
+    context: context,
+    width: AppSize.width * .30,
+    title: title,
+    dialogType: dialogType,
+  ).show();
 }
