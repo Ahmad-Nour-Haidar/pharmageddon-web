1773b170a08693d3956edbfc33c505d2995cc1b5
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2024-02-10 20:40:44 +0300

    some edit2

--- lib/core/constant/app_keys.dart
+++ lib/core/constant/app_keys.dart
@@ -1,10 +1,4 @@
 class AppKeys {
   AppKeys._();
 
-  // static const itemModel = 'itemModel';
-  // static const tag = 'tag';
-  // static const orderModel = 'orderModel';
-// static const tagDate = 'tagDate';
-// static const tagTime = 'tagTime';
-// static const tagNumberPersons = 'tagNumberPersons';
 }
